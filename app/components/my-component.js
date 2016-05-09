f897e299c0d1253cc59c66d90df8dfef30912be9
tyronepost <tyrone.post.a@gmail.com>
2016-05-09 19:44:20 -0400

    adding usecase example of auto-update-input: phoneNumber

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -7,6 +7,16 @@ export default Ember.Component.extend({
     appendZeros(value) {
       value += '000';
       this.set('value', value);
+    },
+
+    insertDash(value) {
+      if (/^\d{10}$/.test(value)) {
+        let areaCode = value.substr(0, 3);
+        let centralOfficeCode = value.substr(3, 3);
+        let lineNumber = value.substr(6, 4);
+        let newValue = areaCode + "-" + centralOfficeCode + "-" + lineNumber;
+        this.set('value', newValue);
+      }
     }
   }
 });
