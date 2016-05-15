1a7997c547a91524b2d88f0ba4781c9625c4a94a
tyronepost <tyrone.post.a@gmail.com>
2016-05-15 10:36:12 -0400

    extracting insert dash logic from action.

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -9,14 +9,24 @@ export default Ember.Component.extend({
       this.set('value', value);
     },
 
-    insertDash(value) {
+    formatPhoneNumber(value) {
       if (/^\d{10}$/.test(value)) {
-        let areaCode = value.substr(0, 3);
-        let centralOfficeCode = value.substr(3, 3);
-        let lineNumber = value.substr(6, 4);
-        let newValue = areaCode + "-" + centralOfficeCode + "-" + lineNumber;
-        this.set('value', newValue);
+       console.log('formatPhoneNumber');
+       let newValue = this.insertDash(value);
+       this.set('value', newValue);
+       this.set('error', undefined);
+      } else {
+        this.set('error', 'incorrect format');
       }
-    }
+
+    },
+  },
+
+  insertDash(value) {
+    let areaCode = value.substr(0, 3);
+    let centralOfficeCode = value.substr(3, 3);
+    let lineNumber = value.substr(6, 4);
+    return areaCode + "-" + centralOfficeCode + "-" + lineNumber;
   }
+
 });
